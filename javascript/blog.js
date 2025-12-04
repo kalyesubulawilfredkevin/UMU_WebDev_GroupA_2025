8258be10219152693d90e6f6a9d46e2929e43f40
Eric-s26 <eric.ssuubi@stud.umu.ac.ug>
2025-12-04 11:14:33 +0300

    final

--- javascript/blog.js
+++ javascript/blog.js
@@ -1,4 +1,4 @@
-// nav bar
+//navbar
 document.addEventListener("DOMContentLoaded", function () {
   const navbarToggler = document.querySelector(".navbar-toggler");
 
@@ -8,3 +8,48 @@ document.addEventListener("DOMContentLoaded", function () {
     navbarCollapse.classList.toggle("show");
   });
 });
+
+//newsletter
+document.addEventListener("DOMContentLoaded", function () {
+  // Newsletter form
+  document.querySelector("form").addEventListener("submit", function (e) {
+    e.preventDefault();
+    const email = this.querySelector('input[type="email"]').value;
+    alert(` Thanks! We'll send blog updates to ${email}`);
+    this.reset();
+  });
+});
+
+// slides
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  slides[slideIndex - 1].style.display = "block";
+}
+// changing interval 3 secs
+setInterval(() => {
+  plusSlides(1);
+}, 3000);
